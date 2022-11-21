9f506aa7eb462a9389875867ca6bfc951cd37b58
Andrey Zhukov <settler59@yandex.ru>
2022-11-21 23:33:58 +0100

    fix: поправил эндпоинты

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -5,7 +5,8 @@ const SECRET_JWT = 'new secret key';
 const allowedCors = [
   'https://praktikum.tk',
   'http://localhost:3000',
-  'http://pioneer.students.nomoredomains.icu/',
+  'http://pioneer.students.nomoredomains.icu',
+  'http://settler.students.nomoredomains.icu',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
